dc62c6479edf6a07cf8620bde8828182bcc92a3b
Daniel <dselasea@gmail.com>
2022-08-26 13:08:10 +0000

    Added Creation component

---
+++ src/images/components/Creation/Creation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Creations.module.css'
+
+const Creation = () => {
+  return (
+    <div className={styles.container}>
+        <h3>our creations</h3>
+        <button className={styles.btn}>See All</button>
+    </div>
+  )
+}
+
+export default Creation
